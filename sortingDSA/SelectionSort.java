4947543744c6e8b76637ec231e910e2e829945ed
anky17 <ankitdiyal17@gmail.com>
2024-01-18 22:51:56 +0545

    selectionsort updated

--- sortingDSA/SelectionSort.java
+++ sortingDSA/SelectionSort.java
@@ -2,33 +2,38 @@ package sortingDSA;
 
 public class SelectionSort {
 
+	// Method to sort the array using selection sort
 	public static void Selectionsorting(int[] arr) {
 
 		int i, j;
-		int n = arr.length;
+		int n = arr.length; // function to get array length
 
 		for (i = 0; i < n - 1; i++) {
-			int minIndex = i;
+			// Initially we assume index = i as smallest sorted array element
+			int minIndex = i; // it store the index of the array pointing to smallest element. 
 			for (j = i; j < n - 1; j++) {
 				if (arr[j] < arr[minIndex]) {
 
-					minIndex = j;
+					minIndex = j; // update the index element smaller than minIndex element is encountered
 
 				}
 			}
-			int temp = arr[minIndex];
+			// swap the position of array element in ascending order
+			int temp = arr[minIndex]; 
 			arr[minIndex] = arr[i];
 			arr[i] = temp;
 		}
 	}
-
+	
+	// Method to print the array
 	public static void printArray(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
-		System.out.println();
+		System.out.println(); // prints the next line
 	}
 
+	// Main function to call the user defined functions 
 	public static void main(String[] args) {
 
 		int[] arr = { 48, 2, 1, 9, 8, 34, 67, 23 };
